bb24ce0233ff873ce80a4f0ab0607699243d0978
ChocOZerO <nayalth@gmail.com>
2017-10-19 19:31:26 +0900

    Add Volume Unit Type

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -38,8 +38,19 @@ struct Weight {
         case oz
     }
     let WeightUnitRate : [String:Double] = [WeightUnit.g.rawValue : 1, WeightUnit.kg.rawValue : 1000, WeightUnit.lb.rawValue : 453.592, WeightUnit.oz.rawValue : 28.3495]
-    let WeightDefaultUnit : [String:String] = [WeightUnit.g.rawValue : WeightUnit.kg.rawValue, WeightUnit.kg.rawValue : WeightUnit.g.rawValue, WeightUnit.lb.rawValue : WeightUnit.kg.rawValue, WeightUnit.oz.rawValue : WeightUnit.kg.rawValue,]
+    let WeightDefaultUnit : [String:String] = [WeightUnit.g.rawValue : WeightUnit.kg.rawValue, WeightUnit.kg.rawValue : WeightUnit.g.rawValue, WeightUnit.lb.rawValue : WeightUnit.kg.rawValue, WeightUnit.oz.rawValue : WeightUnit.kg.rawValue]
     let WeightUnits : [String] = [WeightUnit.kg.rawValue, WeightUnit.g.rawValue, WeightUnit.lb.rawValue, WeightUnit.oz.rawValue]
+}
+struct Volume {
+    enum VolumeUnit : String {
+        case L
+        case pt
+        case qt
+        case gal
+    }
+    let VolumeUnitRate : [String:Double] = [VolumeUnit.L.rawValue : 1, VolumeUnit.pt.rawValue : 0.473176, VolumeUnit.qt.rawValue : 0.946353, VolumeUnit.gal.rawValue : 3.78541]
+    let VolumeDefaultUnit : [String:String] = [VolumeUnit.pt.rawValue : VolumeUnit.L.rawValue, VolumeUnit.qt.rawValue : VolumeUnit.L.rawValue, VolumeUnit.gal.rawValue : VolumeUnit.L.rawValue]
+    let VolumeUnits : [String] = [VolumeUnit.pt.rawValue, VolumeUnit.qt.rawValue, VolumeUnit.gal.rawValue, VolumeUnit.L.rawValue]
 }
 
 struct Unit {
@@ -60,23 +71,20 @@ struct Unit {
 
 let length = Length.init()
 let weight = Weight.init()
+let volume = Volume.init()
 
 func getUnit(target:String) -> String? {
-    var units = length.LengthUnits
-    for item in units {
-        if target.contains(item) {
-            return item
-        }
-    }
-    units = weight.WeightUnits
-    for item in units {
-        if target.contains(item) {
-            return item
+    let unitTypes = [volume.VolumeUnits, weight.WeightUnits, length.LengthUnits]
+    
+    for units in unitTypes {
+        for item in units {
+            if target.contains(item) {
+                return item
+            }
         }
     }
     return nil
 }
-
 
 func getTargetValue(from target: String) -> Double {
     var targetValue : Double = 0.0
@@ -85,7 +93,7 @@ func getTargetValue(from target: String) -> Double {
 }
 
 func printResult(_ result: String) {
-    print("\(result)")
+    print("결과 : \(result)")
 }
 
 func checkUnitType(target: String) -> UnitType {
@@ -95,6 +103,8 @@ func checkUnitType(target: String) -> UnitType {
         unitType = UnitType.Length
     } else if Weight.WeightUnit(rawValue: target) != nil {
         unitType = UnitType.Weight
+    } else if Volume.VolumeUnit(rawValue: target) != nil {
+        unitType = UnitType.Volume
     }
     return unitType
 }
@@ -111,7 +121,7 @@ flag : while true {
     var toFlag : Bool = false
     
     repeat {
-        print("변환시킬 길이 값을 입력해 주세요.")
+        print("변환시킬 값과 단위를 입력해 주세요.")
         input = readLine() ?? ""
         if input == "q" || input == "quit" {
             break flag // 전체 loop 종료
@@ -145,23 +155,33 @@ flag : while true {
         
         if unitTypeTo != unitTypeFrom {
             print("두 단위는 변환할 수 없습니다.")
-            continue
+            toFlag = false
         }
     } while(!toFlag || !fromFlag)
     
+    var unitBase : String
     switch unitTypeTo {
     case UnitType.Length:
-        let lengthUnit = Unit.init(value: value, unitBase: Length.LengthUnit.cm.rawValue, unitRate: length.LengthUnitRate)
+        unitBase = Length.LengthUnit.cm.rawValue
+        let lengthUnit = Unit.init(value: value, unitBase: unitBase, unitRate: length.LengthUnitRate)
         if inputArr.count == 1 {
-            unitTo = length.LengthDefaultUnit[unitFrom] ?? "cm"
+            unitTo = length.LengthDefaultUnit[unitFrom] ?? unitBase
         }
         printResult(String(lengthUnit.convert(from: unitFrom, to: unitTo)) + unitTo)
     case UnitType.Weight:
-        let weightUnit = Unit.init(value: value, unitBase: Weight.WeightUnit.g.rawValue, unitRate: weight.WeightUnitRate)
+        unitBase = Weight.WeightUnit.g.rawValue
+        let weightUnit = Unit.init(value: value, unitBase: unitBase, unitRate: weight.WeightUnitRate)
         if inputArr.count == 1 {
-            unitTo = weight.WeightDefaultUnit[unitFrom] ?? "g"
+            unitTo = weight.WeightDefaultUnit[unitFrom] ?? unitBase
         }
         printResult(String(weightUnit.convert(from: unitFrom, to: unitTo)) + unitTo)
+    case UnitType.Volume:
+        unitBase = Volume.VolumeUnit.L.rawValue
+        let volumeUnit = Unit.init(value: value, unitBase: unitBase, unitRate: volume.VolumeUnitRate)
+        if inputArr.count == 1 {
+            unitTo = volume.VolumeDefaultUnit[unitFrom] ?? unitBase
+        }
+        printResult(String(volumeUnit.convert(from: unitFrom, to: unitTo)) + unitTo)
     default:
         print("입력이 올바르지 않습니다")
         continue
